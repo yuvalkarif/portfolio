048f403db1568d65ab6f000774b30aa0edc66e1e
Yuval Karif <34835592+yuvalkarif@users.noreply.github.com>
2021-10-31 18:17:19 +0200

    contact update

--- src/components/contact.js
+++ src/components/contact.js
@@ -23,7 +23,7 @@ export default function Contact() {
       <div>
         <h2>Contact me</h2>
         <h3>
-          and lets start our journey <strong>together!</strong>
+          and lets start our journey<strong> together!</strong>
         </h3>
         <BottomContainer>
           <CopyToClipboard text={email} onCopy={handleCopied}>
